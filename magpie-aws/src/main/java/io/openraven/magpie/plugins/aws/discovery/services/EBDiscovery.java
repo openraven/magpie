9e05844a79832d06c2ab3ad96bc57da4039877ec
Przemysław Tusiński <przemyslaw@janeasystems.com>
2021-04-08 16:52:01 +0200

    Versioning refactor

--- magpie-aws/src/main/java/io/openraven/magpie/plugins/aws/discovery/services/EBDiscovery.java
+++ magpie-aws/src/main/java/io/openraven/magpie/plugins/aws/discovery/services/EBDiscovery.java
@@ -19,10 +19,10 @@ package io.openraven.magpie.plugins.aws.discovery.services;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import io.openraven.magpie.api.MagpieEnvelope;
+import io.openraven.magpie.api.Emitter;
 import io.openraven.magpie.api.Session;
 import io.openraven.magpie.plugins.aws.discovery.AWSUtils;
-import io.openraven.magpie.plugins.aws.discovery.VersioningEmitterWrapper;
+import io.openraven.magpie.plugins.aws.discovery.VersionedMagpieEnvelopeProvider;
 import org.slf4j.Logger;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.elasticbeanstalk.ElasticBeanstalkClient;
@@ -50,7 +50,7 @@ public class EBDiscovery implements AWSDiscovery {
   }
 
   @Override
-  public void discover(ObjectMapper mapper, Session session, Region region, VersioningEmitterWrapper emitter, Logger logger) {
+  public void discover(ObjectMapper mapper, Session session, Region region, Emitter emitter, Logger logger) {
     final var client = ElasticBeanstalkClient.builder().region(region).build();
 
     getAwsResponse(
@@ -66,7 +66,7 @@ public class EBDiscovery implements AWSDiscovery {
         discoverEnvironmentManagedActions(client, environment, data);
         discoverTags(client, environment, data, mapper);
 
-        emitter.emit(new MagpieEnvelope(session, List.of(fullService() + ":environment"), data));
+        emitter.emit(VersionedMagpieEnvelopeProvider.create(session, List.of(fullService() + ":environment"), data));
       }),
       (noresp) -> logger.error("Failed to get environments in {}", region)
     );
