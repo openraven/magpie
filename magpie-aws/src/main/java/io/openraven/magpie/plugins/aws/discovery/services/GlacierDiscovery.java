5b30d71a97ac74f65ea2de7879c78454d552ad29
Przemysław Tusiński <przemyslaw@janeasystems.com>
2021-04-09 11:48:31 +0200

    Magpie envelope versioning for glacier

--- magpie-aws/src/main/java/io/openraven/magpie/plugins/aws/discovery/services/GlacierDiscovery.java
+++ magpie-aws/src/main/java/io/openraven/magpie/plugins/aws/discovery/services/GlacierDiscovery.java
@@ -19,9 +19,9 @@ package io.openraven.magpie.plugins.aws.discovery.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.openraven.magpie.api.Emitter;
-import io.openraven.magpie.api.MagpieEnvelope;
 import io.openraven.magpie.api.Session;
 import io.openraven.magpie.plugins.aws.discovery.AWSUtils;
+import io.openraven.magpie.plugins.aws.discovery.VersionedMagpieEnvelopeProvider;
 import org.slf4j.Logger;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.glacier.GlacierClient;
@@ -64,7 +64,7 @@ public class GlacierDiscovery implements AWSDiscovery {
         discoverVaultNotifications(client, vault, data);
         discoverTags(client, vault, data);
 
-        emitter.emit(new MagpieEnvelope(session, List.of(fullService() + ":vault"), data));
+        emitter.emit(VersionedMagpieEnvelopeProvider.create(session, List.of(fullService() + ":vault"), data));
       }),
       (noresp) -> logger.error("Failed to get vaults in {}", region)
     );
