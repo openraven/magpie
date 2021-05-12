eb8bdc0a8109d24a4bf72d85fabfc62961cf0b50
Przemysław Tusiński <tusinski.t@gmail.com>
2021-05-12 14:07:20 +0200

    Isolated just magpie-gcp

--- run.sh
+++
@@ -1,7 +0,0 @@
-clear
-mvn clean package install && mvn --projects magpie-cli assembly:single
-cd  magpie-cli/target || exit
-unzip magpie-0.1.0-SNAPSHOT.zip 
-cd magpie-0.1.0-SNAPSHOT/ || exit
-./magpie
-
---
+++ src/main/java/io/openraven/magpie/plugins/gcp/discovery/services/SecretManagerDiscovery.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2021 Open Raven Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.openraven.magpie.plugins.gcp.discovery.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.services.compute.model.Network;
+import com.google.appengine.repackaged.com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.appengine.repackaged.com.google.gson.GsonBuilder;
+import com.google.cloud.secretmanager.v1.ProjectName;
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
+import com.google.cloud.secretmanager.v1beta1.Secret;
+import io.openraven.magpie.api.Emitter;
+import io.openraven.magpie.api.Session;
+import io.openraven.magpie.plugins.gcp.discovery.GCPResource;
+import io.openraven.magpie.plugins.gcp.discovery.VersionedMagpieEnvelopeProvider;
+import org.apache.tinkerpop.gremlin.structure.io.Mapper;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
+
+public class SecretManagerDiscovery {
+
+  public void discover(ObjectMapper mapper, Session session, Emitter emitter) {
+    final String RESOURCE_TYPE = "GCP::SecretManager::Secret";
+
+    final String PROJECT_ID = "oss-discovery-test";
+
+    try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
+      ProjectName projectName = ProjectName.of(PROJECT_ID);
+      
+      SecretManagerServiceClient.ListSecretsPagedResponse pagedResponse = client.listSecrets(projectName);
+      
+      pagedResponse
+        .iterateAll()
+        .forEach(
+          secret -> {
+            Secret s;
+            var data = new GCPResource(mapper);
+            data.resourceType = RESOURCE_TYPE;
+            data.arn = PROJECT_ID +  secret.getName();
+            data.resourceName = secret.getName();
+            data.resourceId = secret.getName();
+            data.configuration = secret.toString();
+
+            emitter.emit(VersionedMagpieEnvelopeProvider.create(session, List.of(":secret"), data.toJsonNode(mapper)));
+          });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
