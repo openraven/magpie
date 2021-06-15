6199268c16ba6adb9967d865e4b0c7f3adf756cf
Jason Nichols <jason@openraven.com>
2021-06-15 16:17:42 -0400

    Moved ignored rules to PolicyContext for easier organization

--- magpie-core/src/main/java/io/openraven/magpie/core/cspm/ScanResults.java
+++ magpie-core/src/main/java/io/openraven/magpie/core/cspm/ScanResults.java
@@ -3,38 +3,11 @@ package io.openraven.magpie.core.cspm;
 import io.openraven.magpie.core.cspm.services.PolicyContext;
 
 import java.util.List;
-import java.util.Map;
 
 public class ScanResults {
 
-  public enum UnscannedReason {
-    DISABLED("Disabled via configuration"),
-    MISSING_ASSET("Asset table not found"),
-    MANUAL_CONTROL("Manual Control");
-
-    private final String reason;
-
-    UnscannedReason(String reason) {
-      this.reason = reason;
-    }
-
-    public String getReason() {
-      return reason;
-    }
-  }
-
-
   private List<PolicyContext> policies = List.of();
   private List<Violation> violations = List.of();
-  private Map<Rule, UnscannedReason> unscannedRules = Map.of();
-
-  public Map<Rule, UnscannedReason> getUnscannedRules() {
-    return unscannedRules;
-  }
-
-  public void setUnscannedRules(Map<Rule, UnscannedReason> unscannedRules) {
-    this.unscannedRules = unscannedRules;
-  }
 
   public List<PolicyContext> getPolicies() {
     return policies;
--- magpie-core/src/main/java/io/openraven/magpie/core/cspm/services/PolicyContext.java
+++ magpie-core/src/main/java/io/openraven/magpie/core/cspm/services/PolicyContext.java
@@ -1,10 +1,39 @@
 package io.openraven.magpie.core.cspm.services;
 
 import io.openraven.magpie.core.cspm.Policy;
+import io.openraven.magpie.core.cspm.Rule;
+
+import java.util.Map;
 
 public class PolicyContext {
+
+  public enum IgnoredReason {
+    DISABLED("Disabled via configuration"),
+    MISSING_ASSET("Asset table not found"),
+    MANUAL_CONTROL("Manual Control");
+
+    private final String reason;
+
+    IgnoredReason(String reason) {
+      this.reason = reason;
+    }
+
+    public String getReason() {
+      return reason;
+    }
+  }
+
   private final PolicyMetadata metadata;
   private final Policy policy;
+  private Map<Rule, IgnoredReason> ignored = Map.of();
+
+  public Map<Rule, IgnoredReason> getIgnored() {
+    return ignored;
+  }
+
+  public void setIgnored(Map<Rule, IgnoredReason> ignoredReason) {
+    this.ignored = ignoredReason;
+  }
 
   public PolicyContext(PolicyMetadata metadata, Policy policy) {
     this.metadata = metadata;
