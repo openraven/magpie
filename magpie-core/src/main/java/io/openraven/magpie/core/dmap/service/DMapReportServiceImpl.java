20a521cb18c9364f571e8cad13bdea306e3c60ac
mbiloshytskyi <belosh59@gmail.com>
2021-07-22 15:13:29 +0300

    #177 Added region to report

--- magpie-core/src/main/java/io/openraven/magpie/core/dmap/service/DMapReportServiceImpl.java
+++ magpie-core/src/main/java/io/openraven/magpie/core/dmap/service/DMapReportServiceImpl.java
@@ -12,10 +12,11 @@ public class DMapReportServiceImpl implements DMapReportService {
 
   private static final String BOLD_SET = "\033[1m";
   private static final String BOLD_RESET = "\033[0m";
+
   private static final int SERVICE_COLUMN_WIDTH = 23;
   private static final int IP_COLUMN_WIDTH = 18;
+  private static final int REGION_COLUMN_WIDTH = 13;
   private static final int PORT_COLUMN_WIDTH = 9;
-  private static final int BEGINING_PORT_COLUMN_WIDTH = 50;
   private static final int PROBABILITY_COLUMN_WIDTH = 14;
   private static final int APP_COLUMN_WIDTH = 15;
 
@@ -25,13 +26,13 @@ public class DMapReportServiceImpl implements DMapReportService {
     System.out.printf("%-30s%-40s\n", "DMap start time", dMapScanResult.getStartDateTime().toString());
     System.out.printf("%-30s%-40s\n\n", "DMap duration", humanReadableFormat(dMapScanResult.getDuration()));
 
-    System.out.printf(BOLD_SET + "%-" + SERVICE_COLUMN_WIDTH + "s%-" + IP_COLUMN_WIDTH + "s%-" + PORT_COLUMN_WIDTH + "s%-" + PROBABILITY_COLUMN_WIDTH + "s%-" + APP_COLUMN_WIDTH + "s\n" + BOLD_RESET,
-      "Resource Id", "IP Address", "Port", "Probability", "App");
+    System.out.printf(BOLD_SET + "%-" + REGION_COLUMN_WIDTH + "s%-" + SERVICE_COLUMN_WIDTH + "s%-" + IP_COLUMN_WIDTH + "s%-" + PORT_COLUMN_WIDTH + "s%-" + PROBABILITY_COLUMN_WIDTH + "s%-" + APP_COLUMN_WIDTH + "s\n" + BOLD_RESET,
+      "Region", "Resource Id", "IP Address", "Port", "Probability", "App");
 
     dMapScanResult.getFingerprintAnalyses().forEach(fingerprintAnalysis -> {
 
-      System.out.printf("%-" + SERVICE_COLUMN_WIDTH + "s%-" + IP_COLUMN_WIDTH + "s",
-        fingerprintAnalysis.getResourceId(), fingerprintAnalysis.getAddress());
+      System.out.printf("%-" + REGION_COLUMN_WIDTH + "s%-" + SERVICE_COLUMN_WIDTH + "s%-" + IP_COLUMN_WIDTH + "s",
+        fingerprintAnalysis.getRegion(), fingerprintAnalysis.getResourceId(), fingerprintAnalysis.getAddress());
 
       if(fingerprintAnalysis.getPredictionsByPort().isEmpty()) {
         System.out.printf("%-"+ PORT_COLUMN_WIDTH + "s%-" + PROBABILITY_COLUMN_WIDTH + "s%-" + APP_COLUMN_WIDTH + "s\n",
@@ -42,7 +43,7 @@ public class DMapReportServiceImpl implements DMapReportService {
 
       new TreeMap<>(fingerprintAnalysis.getPredictionsByPort()).forEach((port, probabilities) -> {
         findTop(probabilities).ifPresent(
-          app -> System.out.printf("%-41s%-"+ PORT_COLUMN_WIDTH + "s%-" + PROBABILITY_COLUMN_WIDTH + ".2f" + "%-4s\n",
+          app -> System.out.printf("%-54s%-"+ PORT_COLUMN_WIDTH + "s%-" + PROBABILITY_COLUMN_WIDTH + ".2f" + "%-4s\n",
             "", port, app.getProbability(), app.getAppName()));
       });
     });
