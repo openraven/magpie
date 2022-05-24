c1e8a546da5d947c90e0d72f0a190402f1f62749
Oliver Ferrigni <oliver@openraven.com>
2022-05-24 11:27:31 -0700

    Disble backup discovery for EB, add exception handling on backup discovery

--- magpie-aws/src/main/java/io/openraven/magpie/plugins/aws/discovery/services/EBDiscovery.java
+++ magpie-aws/src/main/java/io/openraven/magpie/plugins/aws/discovery/services/EBDiscovery.java
@@ -79,7 +79,6 @@ public class EBDiscovery implements AWSDiscovery {
         discoverEnvironmentResources(client, environment, data);
         discoverEnvironmentManagedActions(client, environment, data);
         discoverTags(client, environment, data, mapper);
-        discoverBackupJobs(environment.environmentArn(), region, data, clientCreator, logger);
 
         emitter.emit(VersionedMagpieEnvelopeProvider.create(session, List.of(fullService() + ":environment"), data.toJsonNode()));
       });
