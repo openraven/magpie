82f4c650dbc0582b5a2197b8432c2e3af841896a
michaelgurner <mgurner@openraven.com>
2022-05-24 12:14:23 +0100

    [RAD-448] A number of exceptions that AWS SDK throws are non-exceptional to Magpie, log them and continue

--- magpie-aws/src/main/java/io/openraven/magpie/plugins/aws/discovery/DiscoveryExceptions.java
+++ magpie-aws/src/main/java/io/openraven/magpie/plugins/aws/discovery/DiscoveryExceptions.java
@@ -4,6 +4,7 @@ import io.sentry.Sentry;
 import io.sentry.event.Event;
 import io.sentry.event.EventBuilder;
 import io.sentry.event.interfaces.ExceptionInterface;
+import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
@@ -13,40 +14,78 @@ import software.amazon.awssdk.regions.Region;
 
 public class DiscoveryExceptions {
 
-  private static final Logger logger = LoggerFactory.getLogger(DiscoveryExceptions.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(DiscoveryExceptions.class);
 
   static public void onDiscoveryException(String resourceType, String resourceName, Region region, AwsServiceException ex) {
-    logger.error("{} - AwsServiceException on {} in {}, with error {}", resourceType, resourceName, region, ex.getMessage());
-    Sentry.capture(new EventBuilder().withMessage(resourceType + " AwsServiceException")
-      .withLevel(Event.Level.WARNING)
-      .withFingerprint(String.valueOf(resourceType), String.valueOf(ex.awsErrorDetails()))
-      .withExtra("Resource", String.valueOf(resourceType))
-      .withSentryInterface(new ExceptionInterface(ex)));
+    handleOrReportError(new EventBuilder().withMessage(resourceType + " AwsServiceException")
+      .withLevel(Event.Level.WARNING), resourceType, resourceName, region, ex);
   }
 
   static public void onDiscoveryException(String resourceType, String resourceName, Region region, SdkServiceException ex) {
-    logger.error("{} - SdkServiceException on {} in {}, with error {}", resourceType, resourceName, region, ex.getMessage());
-    Sentry.capture(new EventBuilder().withMessage(resourceType + " SdkServiceException")
-      .withLevel(Event.Level.ERROR)
-      .withExtra("Resource", String.valueOf(resourceType))
-      .withSentryInterface(new ExceptionInterface(ex)));
-
+    handleOrReportError(new EventBuilder().withMessage(resourceType + " SdkServiceException")
+      .withLevel(Event.Level.ERROR), resourceType, resourceName, region, ex);
   }
 
-
   static public void onDiscoveryException(String resourceType, String resourceName, Region region, SdkException ex) {
-    logger.error("{} - SdkException on {} in {}, with error {}", resourceType, resourceName, region, ex.getMessage());
-    Sentry.capture(new EventBuilder().withMessage(resourceType + " SdkException")
-      .withLevel(Event.Level.ERROR)
-      .withExtra("Resource", String.valueOf(resourceType))
-      .withSentryInterface(new ExceptionInterface(ex)));
+    handleOrReportError(new EventBuilder().withMessage(resourceType + " SdkException")
+      .withLevel(Event.Level.ERROR), resourceType, resourceName, region, ex);
   }
 
   static public void onDiscoveryException(String resourceType, String resourceName, Region region, Exception ex) {
-    logger.error("{} - Exception on {} in {}, with error {}", resourceType, resourceName, region, ex.getMessage());
-    Sentry.capture(new EventBuilder().withMessage(resourceType + " Exception")
-      .withLevel(Event.Level.ERROR)
-      .withExtra("Resource", String.valueOf(resourceType))
-      .withSentryInterface(new ExceptionInterface(ex)));
+    handleOrReportError(new EventBuilder().withMessage(resourceType + " Exception")
+      .withLevel(Event.Level.ERROR), resourceType, resourceName, region, ex);
   }
+
+  private static void handleOrReportError(EventBuilder eventBuilder, String resourceType, String resourceName, Region region, Exception ex) {
+    if (!isManagedSdkException(resourceType, resourceName, ex)) {
+      logErrorAndReportToSentry(resourceType, resourceName, region, eventBuilder, ex);
+    }
+  }
+
+  /*
+   * returns true and logs given exception if the exception is an AWS SDK exception that is non-exceptional to Magpie
+   */
+  private static boolean isManagedSdkException(String resourceType, String resourceName, Exception exception) {
+    if ((exception instanceof SdkServiceException) && (((SdkServiceException) exception).isThrottlingException())) {
+      LOGGER.warn("{} - Throttling exception on {}, with error {}", resourceType, resourceName, exception.getMessage());
+      return true;
+    }
+
+    if ((exception instanceof SdkServiceException) && ((SdkServiceException) exception).isClockSkewException()) {
+      LOGGER.warn("{} - Clock skew exception on {}, with error {}", resourceType, resourceName, exception.getMessage());
+      return true;
+    }
+
+    if ((exception instanceof SdkServiceException) && (((SdkServiceException) exception).statusCode() == HttpStatus.SC_NOT_FOUND)) {
+      LOGGER.warn("404 when accessing resource {}", resourceName);
+      return true;
+    }
+
+    if (exception.getMessage().contains("STS is not activated in this region")) {
+      LOGGER.info("STS is not activated in this region for resource {}", resourceName, exception);
+      return true;
+    }
+
+    if (exception.getMessage().contains("not authorized to perform") ||
+      exception.getMessage().contains("AccessDenied") ||
+      exception.getMessage().contains("Access Denied")) {
+      LOGGER.info("Access denied on {}", resourceName);
+      return true;
+    }
+
+    if (exception.getMessage().contains("The AWS Access Key Id needs a subscription for the service")) {
+      LOGGER.info("The AWS Access Key Id needs a subscription for the service for resource {}", resourceName);
+      return true;
+    }
+    return false;
+  }
+
+  private static void logErrorAndReportToSentry(String resourceType, String resourceName, Region region,
+                                                EventBuilder eventBuilder, Exception exception) {
+    LOGGER.error("{} - " + exception.getClass().getTypeName() + " on {} in {}, with error {}", resourceType, resourceName, region, exception.getMessage());
+    Sentry.capture(eventBuilder
+      .withExtra("Resource", String.valueOf(resourceType))
+      .withSentryInterface(new ExceptionInterface(exception)));
+  }
+
 }
