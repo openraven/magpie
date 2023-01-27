c87186115ec5f6e16dd3f7e92880f4ab9c14374f
tg0uld <tgould@openraven.com>
2023-01-27 14:45:55 +0000

    update

--- magpie-gdrive/src/main/java/io/openraven/magpie/plugins/gdrive/discovery/GDriveDiscoveryPlugin.java
+++ magpie-gdrive/src/main/java/io/openraven/magpie/plugins/gdrive/discovery/GDriveDiscoveryPlugin.java
@@ -1,24 +1,22 @@
 package io.openraven.magpie.plugins.gdrive.discovery;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.services.drive.DriveScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import io.openraven.magpie.api.Emitter;
 import io.openraven.magpie.api.OriginPlugin;
 import io.openraven.magpie.api.Session;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import io.openraven.magpie.plugins.gdrive.discovery.services.GDriveDiscovery;
 import io.openraven.magpie.plugins.gdrive.discovery.services.SharedDriveDiscovery;
 import com.google.api.services.drive.Drive;
 import org.slf4j.Logger;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 
 import com.google.api.services.drive.model.DriveList;
 
@@ -31,9 +29,7 @@ import java.util.List;
 public class GDriveDiscoveryPlugin implements OriginPlugin<GDriveDiscoveryConfig> {
   public final static String ID = "magpie.gdrive.discovery";
   protected static final ObjectMapper MAPPER = GDriveUtils.createObjectMapper();
-  private static final java.io.File CREDENTIALS_FOLDER = new java.io.File(System.getProperty("user.home"), "credentials");
-  private static final String CLIENT_SECRET_FILE_NAME = "client_secret.json";
-  private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
+  private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_READONLY);
   private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
 
@@ -61,36 +57,14 @@ public class GDriveDiscoveryPlugin implements OriginPlugin<GDriveDiscoveryConfig
       }));
   }
 
-  private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
-    throws IOException {
-    // Load client secrets.
-
-    java.io.File clientSecretFilePath = new java.io.File(CREDENTIALS_FOLDER, CLIENT_SECRET_FILE_NAME);
-
-    if (!clientSecretFilePath.exists()) {
-      throw new FileNotFoundException("Please copy " + CLIENT_SECRET_FILE_NAME //
-        + " to folder: " + CREDENTIALS_FOLDER.getAbsolutePath());
-    }
-
-    // Load client secrets.
-    InputStream in = new FileInputStream(clientSecretFilePath);
-
-    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-    // Build flow and trigger user authorization request.
-    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-      clientSecrets, SCOPES).setDataStoreFactory(new FileDataStoreFactory(CREDENTIALS_FOLDER))
-      .setAccessType("offline").build();
-
-    return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-  }
-
   //get drive list contained in a workspace
 
   public List<String> getDriveList() throws IOException, GeneralSecurityException {
+
     final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-    Credential credential = getCredentials(HTTP_TRANSPORT);
-    Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).build();
+    ServiceAccountCredentials serviceAccountCredentials = ServiceAccountCredentials.fromStream(new FileInputStream("/Users/tara/credentials/oss-test.json"));
+    HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(serviceAccountCredentials);
+    Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, requestInitializer).build();
     DriveList driveResults = service.drives().list().execute();
     return config.getDriveListProvider().orElse(() -> {
       var drives = new ArrayList<String>();
