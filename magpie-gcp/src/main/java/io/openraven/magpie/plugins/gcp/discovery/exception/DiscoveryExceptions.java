b64f9ff5e746cff8f10ac50c7124a18cb4d222b4
Jason Nichols <jason@kickroot.com>
2023-09-02 12:50:19 -0400

    Fix for PROD-7671

--- magpie-gcp/src/main/java/io/openraven/magpie/plugins/gcp/discovery/exception/DiscoveryExceptions.java
+++ magpie-gcp/src/main/java/io/openraven/magpie/plugins/gcp/discovery/exception/DiscoveryExceptions.java
@@ -1,5 +1,21 @@
+/*
+ * Copyright 2023 Open Raven Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.openraven.magpie.plugins.gcp.discovery.exception;
 
+import io.grpc.StatusRuntimeException;
 import io.sentry.Sentry;
 import io.sentry.SentryEvent;
 import io.sentry.SentryLevel;
@@ -13,6 +29,16 @@ public class DiscoveryExceptions {
 
   static public void onDiscoveryException(String resourceType, Exception ex) {
     logger.error("{} - Exception , with error {}", resourceType, ex.getMessage());
+
+    //
+    // We do not want to sent Sentry events for exceptions caused by StatusRuntimeExceptions indicating the root
+    // cause is a disabled API.
+    // See PROD-7671
+    //
+    if (ex instanceof StatusRuntimeException && ex.getMessage() != null && ex.getMessage().contains("API has not been used")) {
+      return;
+    }
+
     final var event = new SentryEvent();
     event.setLevel(SentryLevel.WARNING);
     final var message = new Message();
