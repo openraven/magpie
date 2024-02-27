41cc4b5caf9eb7adb85cd1ae8604d89396054c29
Oliver Ferrigni <oliver@openraven.com>
2024-02-27 11:05:46 -0800

    Defer credential construction till after tenant id has been resolved and set

--- magpie-azure/src/main/java/io/openraven/magpie/plugins/azure/discovery/AzureDiscoveryPlugin.java
+++ magpie-azure/src/main/java/io/openraven/magpie/plugins/azure/discovery/AzureDiscoveryPlugin.java
@@ -20,6 +20,7 @@ import com.azure.core.implementation.util.EnvironmentConfiguration;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.util.Configuration;
+import com.azure.core.util.ConfigurationBuilder;
 import com.azure.identity.AzureCliCredentialBuilder;
 import com.azure.resourcemanager.AzureResourceManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -33,6 +34,7 @@ import org.slf4j.Logger;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import static com.azure.core.util.Configuration.PROPERTY_AZURE_TENANT_ID;
@@ -86,15 +88,18 @@ public class AzureDiscoveryPlugin implements OriginPlugin<AzureDiscoveryConfig> 
       fetchedCredentials.forEach(mapOfKeysToCredsAndSubInfo -> {
 
         final var subscriptionID = (String)mapOfKeysToCredsAndSubInfo.get("subscription-id");
-        final var credentials = (TokenCredential) mapOfKeysToCredsAndSubInfo.get("credentials");
-        final var configuration = (Configuration) mapOfKeysToCredsAndSubInfo.get("configuration");
+        @SuppressWarnings("unchecked") final var credentialsSupplier = (Supplier<TokenCredential>) mapOfKeysToCredsAndSubInfo.get("credentialsSupplier");
+        final var configurationBuilder = (ConfigurationBuilder) mapOfKeysToCredsAndSubInfo.get("configurationBuilder");
         final var tenantID = (String)mapOfKeysToCredsAndSubInfo.get("tenant-id");
         EnvironmentConfiguration.getGlobalConfiguration().put(PROPERTY_AZURE_TENANT_ID, tenantID);
         final var profile = new AzureProfile(AzureEnvironment.AZURE);
+        final Configuration build = configurationBuilder.build();
+        logger.info("configuration.contains(Configuration.PROPERTY_AZURE_TENANT_ID):{}", build.contains(Configuration.PROPERTY_AZURE_TENANT_ID));
+        logger.info("configuration.get(Configuration.PROPERTY_AZURE_TENANT_ID):{}", build.get(Configuration.PROPERTY_AZURE_TENANT_ID));
         final var azrm = AzureResourceManager
           .configure()
-          .withConfiguration(configuration)
-          .authenticate(credentials, profile)
+          .withConfiguration(build)
+          .authenticate(credentialsSupplier.get(), profile)
           .withTenantId(tenantID)
           .withSubscription(subscriptionID);
 
